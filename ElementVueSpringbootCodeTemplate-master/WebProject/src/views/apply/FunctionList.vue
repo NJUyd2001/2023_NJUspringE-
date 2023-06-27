ba1ddf0f3c1b9982e41361f46488d546c63a6ca1
CL2S <90946191+CL2S@users.noreply.github.com>
2023-06-27 16:54:29 +0800

    'wzx-0627'

--- ElementVueSpringbootCodeTemplate-master/WebProject/src/views/apply/FunctionList.vue
+++ ElementVueSpringbootCodeTemplate-master/WebProject/src/views/apply/FunctionList.vue
@@ -149,9 +149,22 @@ export default {
       // .catch(function (error) { // 请求失败处理
       //   console.log(error);
       // })
-      this.StepNumber+=2;
-      this.info("提交成功，正在返回用户界面！");
-      setTimeout(() => {this.$router.push({path: "./client", replace:true});}, 2000);
+      
+      this.$confirm("是否确认该操作","提示",{
+        iconClass: "el-icon-question",//自定义图标样式
+          confirmButtonText: "确认",//确认按钮文字更换
+          cancelButtonText: "取消",//取消按钮文字更换
+          showClose: true,//是否显示右上角关闭按钮
+          type: "warning",//提示类型  success/info/warning/error
+      }).then(() => {
+          //确认操作
+          this.StepNumber+=2;
+          this.info("提交成功，正在返回用户界面！");
+          setTimeout(() => {this.$router.push({path: "./client", replace:true});}, 2000);
+      })
+      .catch(function (err) {
+        //捕获异常
+      });
     }
   },
 
