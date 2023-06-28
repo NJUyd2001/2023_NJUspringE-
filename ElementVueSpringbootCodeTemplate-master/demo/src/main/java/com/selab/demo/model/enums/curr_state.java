38e63b669ab19266aa3c92d38f816d90f8ec5f09
LiChenbo <201220207@smail.nju.edu.com>
2023-06-28 15:20:43 +0800

    process初步搭建

---
+++ ElementVueSpringbootCodeTemplate-master/demo/src/main/java/com/selab/demo/model/enums/curr_state.java
@@ -0,0 +1,5 @@
+package com.selab.demo.model.enums;
+
+public enum curr_state {
+    R,W,P,A
+}
---
+++ ElementVueSpringbootCodeTemplate-master/demo/src/main/java/com/selab/demo/model/enums/open_to_curr.java
@@ -0,0 +1,5 @@
+package com.selab.demo.model.enums;
+
+public enum open_to_curr {
+    A,B,C,D,E,F,G,H
+}
