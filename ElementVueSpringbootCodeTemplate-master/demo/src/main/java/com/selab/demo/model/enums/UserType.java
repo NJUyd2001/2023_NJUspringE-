2b74df74069d30b1d199cc1887cf33ee456a25b1
LiChenbo <201220207@smail.nju.edu.com>
2023-06-29 14:13:14 +0800

    messages complete

--- ElementVueSpringbootCodeTemplate-master/demo/src/main/java/com/selab/demo/model/enums/UserType.java
+++ ElementVueSpringbootCodeTemplate-master/demo/src/main/java/com/selab/demo/model/enums/UserType.java
@@ -1,7 +1,6 @@
 package com.selab.demo.model.enums;
 
 public enum UserType {
-<<<<<<< HEAD
     C,A,T,M,TM,S;
     /*
     C("顾客"),A("管理员"),T("测试部"),M("市场部"),TM("测试部主管"),S("授权签字人");
@@ -25,9 +24,6 @@ public enum UserType {
     }
     UserType(){}
 */
-=======
-    C,T,M,TL,A,Q
-    // 客户，测试部，市场部，测试部主管，授权签字人，质量部
->>>>>>> 082aec3ea332202577495a1b9b0aea84f9758678
+
 }
 
