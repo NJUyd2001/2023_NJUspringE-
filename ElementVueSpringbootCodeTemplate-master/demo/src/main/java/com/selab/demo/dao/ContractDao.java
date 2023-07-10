cdf4932d52c374c3f3d00b408f317fc6c806ca20
neoabridges <631733050@qq.com>
2023-07-10 11:46:44 +0800

    lxa-710

---
+++ ElementVueSpringbootCodeTemplate-master/demo/src/main/java/com/selab/demo/dao/ContractDao.java
@@ -0,0 +1,23 @@
+package com.selab.demo.dao;
+import com.selab.demo.model.ContractModel;
+import org.apache.ibatis.annotations.*;
+
+import java.util.List;
+@Mapper
+public interface ContractDao {
+    @Insert("INSERT INTO selabspringe.contract (itemname,client,trustee,qc,date,periodofvalidity,ddl,changenumber,changeday,money) VALUES(#{itemname},#{client},#{trustee},#{qc},#{date},#{periodofvalidity},#{ddl},#{changenumber},#{changeday},#{money})")
+    @Options(useGeneratedKeys=true, keyProperty="CID", keyColumn="CID")
+    public void insert(ContractModel contractModel);
+
+    @Select("SELECT * FROM selabspringe.contract WHERE CID = #{CID}")
+    public List<ContractModel> select(Integer CID);
+
+    @Select("SELECT CID FROM selabspringe.contract WHERE CID = #{CID}")
+    public Integer select2(Integer CID);
+
+    @Update("UPDATE selabspringe.contract SET itemname = #{itemname},client = #{client},trustee = #{trustee},qc = #{qc},date = #{date},periodofvalidity = #{periodofvalidity},ddl = #{ddl},changenumber = #{changenumber},changeday = #{changeday},money=#{money} WHERE CID = #{CID}")
+    public void update(ContractModel contractModel);
+
+    @Delete("DELETE FROM selabspringe.contract WHERE CID = #{CID}")
+    public void delete(Integer CID);
+}
---
+++ ElementVueSpringbootCodeTemplate-master/docs/md/Contract接口.md
@@ -0,0 +1,93 @@
+# Contract接口
+author：李晨博
+
+## /contract/insert
+插入一个contract
+
+输入示例：
+```
+{
+    "PID":"1",
+    "ItemName":"",
+    "Client":"豪大大2鸡排",
+    "Trustee":"''",
+    "QC":"''",
+    "date":"''",
+    "PeriodOfValidity":"''",
+    "ddl":"0",
+    "ChangeNumber":"0",
+    "ChangeDay":"0",
+    "money":"1"
+}
+
+输出示例1：
+```
+the process does not exist
+```
+输出示例2：
+```
+{"CID":"1","PID","1"}
+```
+
+## /contract/find
+查找一个contract
+
+输入示例：
+```
+{
+    "PID":"1"
+}
+```
+输出示例：
+```
+[{"CID":"1","ItemName":"","Client":"豪大大鸡排","Trustee":"''","QC":"''","date":"''",
+"PeriodOfValidity":"''","ddl":"0","ChangeNumber":"0","ChangeDay":"0"}]
+
+```
+
+## /contract/update
+更新contract
+
+输入示例:
+```
+{
+    "PID":"1",
+    "ItemName":"",
+    "Client":"豪大大2鸡排",
+    "Trustee":"''",
+    "QC":"''",
+    "date":"''",
+    "PeriodOfValidity":"''",
+    "ddl":"0",
+    "ChangeNumber":"0",
+    "ChangeDay":"0"
+}
+
+```
+
+输出示例1：
+```
+the contract does not exist
+```
+输出示例2：
+```
+contract update complete
+```
+
+## /contract/delete
+删除contract
+
+输入示例：
+```
+{
+    "PID":"1"
+}
+```
+输出示例1：
+```
+the contract does not exist
+```
+输出示例2：
+```
+contract delete complete
+```
